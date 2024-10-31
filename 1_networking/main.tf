eb3ccd4018bd8403720b09bee49c5acfee992e3b
Jake Durocher <jake.durocher@jake.durocher-HPXL64FXJY>
2024-10-31 13:43:50 -0500

    adding private subnets to HVN peering

--- 1_networking/main.tf
+++ 1_networking/main.tf
@@ -68,3 +68,13 @@ module "aws_hcp_network_config" {
   subnet_ids      = module.vpc.public_subnets
   route_table_ids = module.vpc.public_route_table_ids
 }
+
+module "aws_private_hcp_network_config" {
+  source  = "hashicorp/hcp-consul/aws"
+  version = "~> 0.12.1"
+
+  hvn             = hcp_hvn.main
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+  route_table_ids = module.vpc.private_route_table_ids
+}
